0745247c5664b5de514c0c499247a5310b933ba4
spragala <spragala@yahoo.com>
2017-05-30 16:50:36 -0700

    PUT route working, data displaying

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,30 +8,30 @@
 
 /* hard-coded data! */
 var allAlbums = [];
-allAlbums.push({
-             artistName: 'Ladyhawke',
-             name: 'Ladyhawke',
-             releaseDate: '2008, November 18',
-             genres: [ 'new wave', 'indie rock', 'synth pop' ]
-           });
-allAlbums.push({
-             artistName: 'The Knife',
-             name: 'Silent Shout',
-             releaseDate: '2006, February 17',
-             genres: [ 'synth pop', 'electronica', 'experimental' ]
-           });
-allAlbums.push({
-             artistName: 'Juno Reactor',
-             name: 'Shango',
-             releaseDate: '2000, October 9',
-             genres: [ 'electronic', 'goa trance', 'tribal house' ]
-           });
-allAlbums.push({
-             artistName: 'Philip Wesley',
-             name: 'Dark Night of the Soul',
-             releaseDate: '2008, September 12',
-             genres: [ 'piano' ]
-           });
+// allAlbums.push({
+//              artistName: 'Ladyhawke',
+//              name: 'Ladyhawke',
+//              releaseDate: '2008, November 18',
+//              genres: [ 'new wave', 'indie rock', 'synth pop' ]
+//            });
+// allAlbums.push({
+//              artistName: 'The Knife',
+//              name: 'Silent Shout',
+//              releaseDate: '2006, February 17',
+//              genres: [ 'synth pop', 'electronica', 'experimental' ]
+//            });
+// allAlbums.push({
+//              artistName: 'Juno Reactor',
+//              name: 'Shango',
+//              releaseDate: '2000, October 9',
+//              genres: [ 'electronic', 'goa trance', 'tribal house' ]
+//            });
+// allAlbums.push({
+//              artistName: 'Philip Wesley',
+//              name: 'Dark Night of the Soul',
+//              releaseDate: '2008, September 12',
+//              genres: [ 'piano' ]
+//            });
 /* end of hard-coded data */
 
 
@@ -40,54 +40,7 @@ allAlbums.push({
 $(document).ready(function() {
   console.log('app.js loaded!');
 
-  for (let i = 0; i < allAlbums.length; i++) {
-
-   function getHtml(){
-    return `<div class='row'>
-      <div class="col-md-3 col-xs-12 thumbnail album-art">
-        <img src="/images/800x800.png" alt="album image">
-      </div>
-
-      <div class="col-md-9 col-xs-12">
-        <ul class="list-group">
-          <li class="list-group-item">
-            <h4 class='inline-header'>Album Name:</h4>
-            <span class='album-name'>${allAlbums[i].name}</span>
-          </li>
-
-          <li class="list-group-item">
-            <h4 class='inline-header'>Artist Name:</h4>
-            <span class='artist-name'>${allAlbums[i].artistName}</span>
-          </li>
-
-          <li class="list-group-item">
-            <h4 class='inline-header'>Released date:</h4>
-            <span class='album-releaseDate'>${allAlbums[i].releaseDate}</span>
-          </li>
-        </ul>
-      </div>
-
-    </div>`
-    }
-  let albumsData = getHtml();
-  $('.panel-body').append(albumsData);
-  };
-
-  $.ajax({
-    method: 'GET',
-    url: '/api/album',
-    success: getAllAlbums,
-    error: handleAlbumsError
-  });
-
-  function getAllAlbums(json){
-    console.log(json);
-    allAlbums = json;
-  }
-
-  function handleAlbumsError(){
-    console.log("Problem fetching data.");
-  }
+  let $albumList = $('.panel-body')
 
   $('form').on('submit', function(event){
     console.log("this is the event" + event)
@@ -106,10 +59,67 @@ $(document).ready(function() {
     $('#album-form input').val('');
     allAlbums.push(json);
     console.log(allAlbums)
+    render();
   }
 
   function createError(){
     console.log("error creating.")
+  }
+
+   function getHtml(allAlbums){
+    return `<div class='row'>
+      <div class="col-md-3 col-xs-12 thumbnail album-art">
+        <img src="/images/800x800.png" alt="album image">
+      </div>
+
+      <div class="col-md-9 col-xs-12">
+        <ul class="list-group">
+          <li class="list-group-item">
+            <h4 class='inline-header'>Album Name:</h4>
+            <span class='album-name'>${allAlbums.name}</span>
+          </li>
+
+          <li class="list-group-item">
+            <h4 class='inline-header'>Artist Name:</h4>
+            <span class='artist-name'>${allAlbums.artistName}</span>
+          </li>
+
+          <li class="list-group-item">
+            <h4 class='inline-header'>Released date:</h4>
+            <span class='album-releaseDate'>${allAlbums.releaseDate}</span>
+          </li>
+        </ul>
+      </div>
+
+    </div>`
+    }
+
+
+  $.ajax({
+    method: 'GET',
+    url: '/api/album',
+    success: getAllAlbums,
+    error: handleAlbumsError
+  });
+
+  function getAllAlbums(json){
+    console.log(json);
+    allAlbums = json;
+    render();
+  }
+
+  function handleAlbumsError(){
+    console.log("Problem fetching data.");
+  }
+
+  function render() {
+    $albumList.empty();
+    var restsHtml = getTheAlbums(allAlbums);
+    $albumList.append(restsHtml);
+  };
+
+  function getTheAlbums(potato) {
+    return potato.map(getHtml).join("")
   }
 
 
@@ -125,7 +135,7 @@ $(document).ready(function() {
 
 
 // this function takes a single album and renders it to the page
-function renderAlbum(album) {
-  console.log('rendering album:', album);
-
-}
+// function renderAlbum(album) {
+//   console.log('rendering album:', album);
+//
+// }
